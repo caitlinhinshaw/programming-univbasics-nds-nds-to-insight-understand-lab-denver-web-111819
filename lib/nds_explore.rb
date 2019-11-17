6e3fc94c0bc271064938139edd21ad96a8108676
Caitlin Hinshaw <caitlin.hinshaw@gmail.com>
2019-11-17 04:47:01 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -9,6 +9,36 @@ end
 
 def print_first_directors_movie_titles
 
+  movie_index = 0
+  while movie_index < directors_database[0].length do
+    puts "#{directors_database[0][movie_index]}\n"
+    movie_index +=1
+  end
+  
+end
+=begin
+  row_index = 0
+  while row_index < vm.length do
+    puts "Row #{row_index} has #{vm[row_index]} columns"
+   
+    column_index = 0
+    while column_index < vm[row_index].length do
+      coord = "#{row_index}, #{column_index}"
+      inner_len = vm[row_index][column_index].length
+      # Remember \t is a TAB character for indentation
+      puts "\tCoordinate [#{coord}] points to an #{vm[row_index][column_index].class} of length #{inner_len}"
+   
+      inner_index = 0
+      while inner_index < inner_len do
+        puts "\t\t (#{coord}, #{inner_index}) is: #{vm[row_index][column_index][inner_index]}"
+        inner_index += 1
+      end
+   
+      column_index += 1
+    end
+   
+    row_index += 1
+  end
 end
 
 
@@ -28,3 +58,4 @@ directors_database[0][:loop through titles]
      :studio=>"Paramount",
      :worldwide_gross=>248159971,
      :release_year=>1981},
+=end
