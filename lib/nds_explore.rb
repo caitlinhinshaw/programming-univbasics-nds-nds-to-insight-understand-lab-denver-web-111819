756bf5f3b75226e085f139aac65bdfb6005f4e64
Caitlin Hinshaw <caitlin.hinshaw@gmail.com>
2019-11-17 04:37:53 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -5,11 +5,26 @@ require 'directors_database'
 def pretty_print_nds(nds)
   # Change the code below to pretty print the nds with pp
   pp(nds)
-  puts pp(nds)
 end
 
 def print_first_directors_movie_titles
 
 end
 
-pretty_print_nds(directors_database)
+
+directors_database[0][:loop through titles]
+
+[{:name=>"Stephen Spielberg",
+  :movies=>
+   [{:title=>"Jaws",
+     :studio=>"Universal",
+     :worldwide_gross=>260000000,
+     :release_year=>1975},
+    {:title=>"Close Encounters of the Third Kind",
+     :studio=>"Columbia",
+     :worldwide_gross=>135189114,
+     :release_year=>1977},
+    {:title=>"Raiders of the Lost Ark",
+     :studio=>"Paramount",
+     :worldwide_gross=>248159971,
+     :release_year=>1981},
